62f36b66583736ebbb85a908377f10f51126b0a0
Vlad <peckovychv@gmail.com>
2024-09-12 21:06:28 +0300

    server

--- server/serverN.js
+++ server/serverN.js
@@ -1,16 +1,16 @@
-require('./db')
+require("./db");
 
 const express = require("express");
 const app = express();
 const port = 3004;
 
-const userRouter = require("./routes/user");
-
-app.use('/user', userRouter)
-
 app.get("/", (req, res) => {
   res.send("кореневий шлях нотатника");
 });
+
+const userRouter = require("./routes/user");
+
+app.use("/user", userRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
